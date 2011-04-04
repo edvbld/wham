8f07617adaf06084be07a49926fc3edb63c08764
helino <erik.helin@gmail.com>
2011-04-04 21:02:32 +0200

    added boolean expressions

--- Parser.hs
+++ Parser.hs
@@ -11,49 +11,102 @@ lexer :: P.TokenParser ()
 lexer = P.makeTokenParser whileDef
 
 whileDef = (emptyDef
-              {P.reservedOpNames = ["*", "+", "-"],
+              {P.reservedOpNames = ["*", "+", "-", "!", "&", "=", "<="],
+               P.reservedNames = ["true", "false"],
                P.identStart = letter
               })
 
 identifier = P.identifier lexer
 reservedOp = P.reservedOp lexer
+reserved = P.reserved lexer
 integer = P.integer lexer
 
 -- parser 
 
-data AExp = Number Integer
-          | Variable String
-          | Add AExp AExp
-          | Mul AExp AExp
-          | Sub AExp AExp
-            deriving (Show)
+data ArithmeticExp = Number Integer
+                   | Variable String
+                   | Add ArithmeticExp ArithmeticExp
+                   | Mul ArithmeticExp ArithmeticExp
+                   | Sub ArithmeticExp ArithmeticExp
+                   deriving (Show)
 
-aexp :: Parser AExp
-aexp = buildExpressionParser table arithmetic
+data BooleanExp = Boolean Bool
+                | Not BooleanExp
+                | And BooleanExp BooleanExp
+                | Equal ArithmeticExp ArithmeticExp
+                | LessOrEqual ArithmeticExp ArithmeticExp
+                deriving (Show)
 
-arithmetic :: Parser AExp
+aexp = buildExpressionParser arithmeticOperators arithmetic
+bexp = buildExpressionParser booleanOperators boolean
+
+boolean :: Parser BooleanExp
+boolean =   true 
+        <|> false
+        <|> arithmeticComparison
+
+booleanOperators :: OperatorTable Char () BooleanExp
+booleanOperators = [[prefix "!" Parser.not],
+                    [binary "&" Parser.and AssocLeft]]
+
+arithmeticComparison :: Parser BooleanExp
+arithmeticComparison = do { l <- aexp;
+                            do { reservedOp "=";
+                                 equals l}
+                          <|>
+                            do { reservedOp "<=";
+                                 lessOrEquals l}
+                           }
+
+equals :: ArithmeticExp -> Parser BooleanExp
+equals l  = do r <- aexp
+               return (Equal l r)
+
+lessOrEquals :: ArithmeticExp -> Parser BooleanExp
+lessOrEquals l = do r <- aexp
+                    return (LessOrEqual l r)
+
+not :: BooleanExp -> BooleanExp
+not b = Not b
+
+and :: BooleanExp -> BooleanExp -> BooleanExp
+and l r = And l r
+
+true :: Parser BooleanExp
+true = do reserved "true"
+          return (Boolean True)
+
+false :: Parser BooleanExp
+false = do reserved "false"
+           return (Boolean False)
+
+arithmetic :: Parser ArithmeticExp
 arithmetic = number <|> variable
 
-number :: Parser AExp
+number :: Parser ArithmeticExp
 number = do n <- integer
             return (Number n)
 
-variable :: Parser AExp
+variable :: Parser ArithmeticExp
 variable = do var <- identifier
               return (Variable var)
 
-table :: OperatorTable Char () AExp
-table = [[binary "*" mul AssocLeft],
-         [binary "+" add AssocLeft, binary "-" sub AssocLeft]]
+arithmeticOperators :: OperatorTable Char () ArithmeticExp
+arithmeticOperators = [[binary "*" mul AssocLeft],
+                       [binary "+" add AssocLeft, binary "-" sub AssocLeft]]
 
 binary symbol operation assoc = Infix (do {reservedOp symbol; 
                                            return operation}) 
                                 assoc
-mul :: AExp -> AExp -> AExp
+
+prefix symbol operation = Prefix (do { reservedOp symbol; 
+                                       return operation })
+
+mul :: ArithmeticExp -> ArithmeticExp -> ArithmeticExp 
 mul l r = Mul l r
 
-add :: AExp -> AExp -> AExp
+add :: ArithmeticExp -> ArithmeticExp -> ArithmeticExp 
 add l r = Add l r
 
-sub :: AExp -> AExp -> AExp
+sub :: ArithmeticExp -> ArithmeticExp -> ArithmeticExp 
 sub l r = Sub l r
