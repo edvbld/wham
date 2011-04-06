8b926042dfdca63b73f331c517a6165e4c83977d
helino <erik.helin@gmail.com>
2011-04-06 22:34:00 +0200

    fixed order of translated arguments

--- Translator.hs
+++ Translator.hs
@@ -26,8 +26,8 @@ translate s = translateStatement s
 translateArithmetic :: ArithmeticExp -> [AMExpression]
 translateArithmetic (Number n) = [(PUSH n)]
 translateArithmetic (Variable x) = [(FETCH x)]
-translateArithmetic (Add a1 a2) = (translateArithmetic a1) ++  
-                                  (translateArithmetic a2) ++
+translateArithmetic (Add a1 a2) = (translateArithmetic a2) ++  
+                                  (translateArithmetic a1) ++
                                   [ADD]
 translateArithmetic (Sub a1 a2) = (translateArithmetic a2) ++  
                                   (translateArithmetic a1) ++
@@ -43,14 +43,14 @@ translateBoolean :: BooleanExp -> [AMExpression]
 translateBoolean (Boolean True) = [TRUE]
 translateBoolean (Boolean False) = [FALSE]
 translateBoolean (Not b) = (translateBoolean b) ++ [NEG]
-translateBoolean (Equal a1 a2) = (translateArithmetic a1) ++
-                                 (translateArithmetic a2) ++
+translateBoolean (Equal a1 a2) = (translateArithmetic a2) ++
+                                 (translateArithmetic a1) ++
                                  [EQUAL]
-translateBoolean (LessOrEqual a1 a2) = (translateArithmetic a1) ++
-                                       (translateArithmetic a2) ++
+translateBoolean (LessOrEqual a1 a2) = (translateArithmetic a2) ++
+                                       (translateArithmetic a1) ++
                                        [LE]
-translateBoolean (And b1 b2) = (translateBoolean b1) ++
-                               (translateBoolean b2) ++
+translateBoolean (And b1 b2) = (translateBoolean b2) ++
+                               (translateBoolean b1) ++
                                [AND]
 
 translateStatement :: Statement -> [AMExpression]
