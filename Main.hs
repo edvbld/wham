c283771dc2f8f0ba02fa7e25e00a5220ab72d762
helino <erik.helin@gmail.com>
2011-04-07 12:09:34 +0200

    added two command line options, debug is not yet implemented

--- Main.hs
+++ Main.hs
@@ -1,15 +1,41 @@
 module Main(main) where
 
-import System.Environment
+import System.Environment (getArgs)
+import Data.Maybe (fromMaybe)
+import Data.List (sort)
+import System.Console.GetOpt
 import Parser
 import Translator
 import Interpreter
 
 main :: IO()
 main = do args <- getArgs
-          if (length args) /= 1
-            then putStrLn "usage: wham <name-of-file>"
-            else do content <- readFile (head args)
-                    case parse content of
-                        Right stmt -> print (evaluate (translate stmt) [])
-                        Left err -> print err
+          case getOpt RequireOrder options args of
+            (flags, [fname], []) -> do content <- readFile fname
+                                       eval (sort flags) content
+            (_, _, msgs) -> error $ concat msgs ++ usageInfo header options
+          where
+
+eval :: [Flag] -> String -> IO()
+eval [] content = run content [] False
+eval [State s] content = run content s False
+eval [Debug] content = run content [] True
+eval [Debug, State s] content = run content s True
+
+run :: String -> [(String, Integer)] -> Bool -> IO()
+run content state debug = case parse content of 
+    Right stmt -> print (evaluate (translate stmt) state)
+    Left err -> print err
+
+
+data Flag = Debug | State [(String, Integer)] deriving (Eq, Show, Ord)
+
+options :: [OptDescr Flag]
+options = [Option ['d'] ["debug"] (NoArg Debug) "show debug info",
+           Option ['s'] ["state"] (ReqArg makeState "state") "evaluate from the given state"]
+
+makeState :: String -> Flag
+makeState s = State ((read s)::[(String, Integer)])
+
+header :: String
+header = "[OPTIONS] file-name"
