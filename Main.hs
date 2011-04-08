cb58122fe049f6dbfff7941c33631d41596a5330
helino <erik.helin@gmail.com>
2011-04-08 14:49:59 +0200

    implemented the debug option

--- Main.hs
+++ Main.hs
@@ -23,16 +23,29 @@ eval [Debug] content = run content [] True
 eval [Debug, State s] content = run content s True
 
 run :: String -> [(String, Integer)] -> Bool -> IO()
-run content state debug = case parse content of 
-    Right stmt -> print (evaluate (translate stmt) state)
+run content state shouldDebug = case parse content of 
+    Right stmt -> do let exps = translate stmt
+                     if shouldDebug
+                        then debug exps state
+                        else print (evaluate (translate stmt) state)
     Left err -> print err
 
+debug :: [AMExpression] -> [(String, Integer)] -> IO()
+debug exps list = impl (exps, [], (state list))
+    where
+        impl ([], _, state) = return ()
+        impl c = do let c' = step c
+                    print c'
+                    getLine
+                    impl c'
 
 data Flag = Debug | State [(String, Integer)] deriving (Eq, Show, Ord)
 
 options :: [OptDescr Flag]
-options = [Option ['d'] ["debug"] (NoArg Debug) "show debug info",
-           Option ['s'] ["state"] (ReqArg makeState "state") "evaluate from the given state"]
+options = [Option ['d'] ["debug"] (NoArg Debug) 
+           "show debug info",
+           Option ['s'] ["state"] (ReqArg makeState "state") 
+           "evaluate from the given state"]
 
 makeState :: String -> Flag
 makeState s = State ((read s)::[(String, Integer)])
