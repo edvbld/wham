27cdca9b4c6e58c7408d56e43b5c2641c60c257b
helino <erik.helin@gmail.com>
2011-04-08 09:56:59 +0200

    changed the state of the AM to support both Normal and Exception states

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,52 +1,74 @@
-module Interpreter(evaluate) where
+module Interpreter(evaluate, StateMode(..)) where
 
 import qualified Data.Map as Map
 import Translator
 
 data StackElement = Integer Integer | Bool Bool 
 type Stack = [StackElement]
-type AMState = Map.Map String Integer
+data StateMode = Normal 
+               | Exception 
+                 deriving (Eq, Ord, Show)
+type State = Map.Map String Integer
+type EState = (State, StateMode)
 
-evaluate :: [AMExpression] -> [(String, Integer)] -> [(String, Integer)]
-evaluate exps state = Map.toList (eval exps [] (Map.fromList state))
-
-eval :: [AMExpression] -> Stack -> AMState -> AMState
+evaluate :: [AMExpression] -> [(String, Integer)] -> 
+            ([(String, Integer)], StateMode)
+evaluate exps state = (stateList, mode)
+    where
+        (finalState, mode) = eval exps [] ((Map.fromList state), Normal)
+        stateList = Map.toList finalState
+ 
+eval :: [AMExpression] -> Stack -> EState -> EState
 eval [] stack state = state
-eval ((PUSH n):exps) stack state = eval exps ((Integer n):stack) state
-eval ((STORE x):exps) ((Integer n):stack) state = eval exps stack state'
-    where state' = update x n state 
-eval ((FETCH x):exps) stack state = eval exps stack' state
+eval ((PUSH n):exps) stack (state, Normal) = 
+    eval exps ((Integer n):stack) (state, Normal)
+eval ((STORE x):exps) ((Integer n):stack) (state, Normal) = 
+    eval exps stack (state', Normal)
+        where state' = update x n state 
+eval ((FETCH x):exps) stack (state, Normal) = eval exps stack' (state, Normal)
     where stack' = ((Interpreter.lookup x state):stack) 
-eval (NOOP:exps) stack state = eval exps stack state
-eval (ADD:exps) ((Integer a):(Integer b):stack) state = eval exps stack' state
-    where stack' = ((Integer (a+b)):stack)
-eval (SUB:exps) ((Integer a):(Integer b):stack) state = eval exps stack' state
-    where stack' = ((Integer (a-b)):stack)
-eval (MULT:exps) ((Integer a):(Integer b):stack) state = eval exps stack' state
-    where stack' = ((Integer (a*b)):stack)
-eval (DIV:exps) ((Integer a):(Integer b):stack) state = eval exps stack' state
-    where stack' = ((Integer (div a b)):stack)
-eval (TRUE:exps) stack state = eval exps ((Bool True):stack) state
-eval (FALSE:exps) stack state = eval exps ((Bool False):stack) state
-eval (EQUAL:exps) ((Integer a):(Integer b):stack) state = eval exps stack' state
-    where stack' = ((Bool (a == b)):stack)
-eval (LE:exps) ((Integer a):(Integer b):stack) state = eval exps stack' state
-    where stack' = ((Bool (a <= b)):stack)
-eval (AND:exps) ((Bool a):(Bool b):stack) state = eval exps stack' state
-    where stack' = ((Bool (a && b)):stack)
-eval (NEG:exps) ((Bool a):stack) state = eval exps stack' state
-    where stack' = ((Bool (not a)):stack)
-eval ((BRANCH s1 s2):exps) ((Bool b):stack) state = eval exps' stack state
-    where exps' = if b then s1 ++ exps else s2 ++ exps
-eval ((LOOP b s):exps) stack state = eval exps' stack state
-    where exps' = b ++ [(BRANCH (s ++ [LOOP b s]) [NOOP])] ++ exps
+eval (NOOP:exps) stack (state, Normal) = eval exps stack (state, Normal)
+eval (ADD:exps) ((Integer a):(Integer b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Integer (a+b)):stack)
+eval (SUB:exps) ((Integer a):(Integer b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Integer (a-b)):stack)
+eval (MULT:exps) ((Integer a):(Integer b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Integer (a*b)):stack)
+eval (DIV:exps) ((Integer a):(Integer b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Integer (div a b)):stack)
+eval (TRUE:exps) stack (state, Normal) = 
+    eval exps ((Bool True):stack) (state, Normal)
+eval (FALSE:exps) stack (state, Normal) = 
+    eval exps ((Bool False):stack) (state, Normal)
+eval (EQUAL:exps) ((Integer a):(Integer b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Bool (a == b)):stack)
+eval (LE:exps) ((Integer a):(Integer b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Bool (a <= b)):stack)
+eval (AND:exps) ((Bool a):(Bool b):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Bool (a && b)):stack)
+eval (NEG:exps) ((Bool a):stack) (state, Normal) = 
+    eval exps stack' (state, Normal)
+        where stack' = ((Bool (not a)):stack)
+eval ((BRANCH s1 s2):exps) ((Bool b):stack) (state, Normal) = 
+    eval exps' stack (state, Normal)
+        where exps' = if b then s1 ++ exps else s2 ++ exps
+eval ((LOOP b s):exps) stack (state, Normal) = 
+    eval exps' stack (state, Normal)
+        where exps' = b ++ [(BRANCH (s ++ [LOOP b s]) [NOOP])] ++ exps
 
-update :: String -> Integer -> AMState -> AMState
+update :: String -> Integer -> State -> State
 update x n s = 
     case Map.member x s of
         True -> Map.update (\_ -> Just n) x s
         False -> Map.insert x n s
 
-lookup :: String -> AMState -> StackElement
+lookup :: String -> State -> StackElement
 lookup x s = case Map.lookup x s of
                 Just n -> (Integer n)
